b0e993c42c63af4b71d82b618ec1531eb0b20ef1
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-09-10 16:23:03 -0400

    Added created_at field to graphql message type

--- app/graphql/types/message_type.rb
+++ app/graphql/types/message_type.rb
@@ -4,6 +4,7 @@ module Types
   class MessageType < Types::BaseObject
     field :id, ID, null: true
     field :content, String, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: true
     field :chat, ChatType, null: true
     field :user, UserType, null: true
   end
